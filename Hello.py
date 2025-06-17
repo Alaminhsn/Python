a75eca922e2df3827ea149c9f390cab0bef22dc0
Alamin Hosen <alaminhsn98@gmail.com>
2025-06-17 23:50:54 +0600

    Fist program

---
+++ Hello.py
@@ -0,0 +1,26 @@
+print("Hello world!")
+
+# 1.Variable
+Name="My name is Alamin"
+print(Name)
+Number=1345
+
+# 2.Data type
+print(type(Number))
+
+Number1="1345"
+print(type(Number1))
+
+Number2 =3.55
+print(type(Number2))
+
+list_variable2=(1,2,3,44)
+print(type(list_variable2))
+
+#List
+list_variable=[1,3,5,6]
+print(type(list_variable))
+print(list_variable)
+list_variable.append(7)
+print(list_variable)
+
