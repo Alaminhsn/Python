cb0ed6aa189742be534cc4df9c9a8700c2206c71
Alamin Hosen <alaminhsn98@gmail.com>
2025-06-17 23:37:42 +0600

    aother file

---
+++ function.py
@@ -0,0 +1,16 @@
+def add(x,y):
+    sum=x+y
+    print(sum)
+
+add(50,40)
+
+def sub(a,b):
+    result=a-b
+    print(result)
+
+sub(90,40)
+
+def message():
+    print("I am learning Python")
+
+message()
