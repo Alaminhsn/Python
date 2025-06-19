932b135fe14f409f81fdfc2a56a459043357e4e8
Alamin Hosen <alaminhsn98@gmail.com>
2025-06-19 15:55:10 +0600

    class

---
+++ class.py
@@ -0,0 +1,16 @@
+class Student:
+    id=""
+    gpa=""
+    dep=""
+
+sakib = Student()
+sakib.id=960
+sakib.gpa=3.50
+sakib.dep="cse"
+print(f"ID:{sakib.id},GPA:{sakib.gpa},Department:{sakib.dep}")
+
+Nahid = Student()
+Nahid.id=988
+Nahid.gpa=3.80
+Nahid.dep="cse"
+print(f"ID:{Nahid.id},GPA:{Nahid.gpa},Department:{Nahid.dep}")
